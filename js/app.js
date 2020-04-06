37808ecb09e2276f2287e4879e929375769a196c
Judy <kimjudyh@gmail.com>
2020-04-05 23:41:11 -0700

    made function to set up playing field

--- js/app.js
+++ js/app.js
@@ -1,5 +1,42 @@
 console.log('bunny javascript working');
 
+// fill playing field with holes
+const makeLevel = (level) => {
+  /* 
+    <div class='hole-area'>
+      <img class = 'hole' src="img/hole.svg" alt="hole in ground">
+      <img class='bunny' src="img/bunny.svg" alt="bunny">
+    </div>
+  */
+  const levelMakerObject = {
+    1 : 16,
+    2 : 25
+  };
+  // level 1: 4x4 grid
+  // grab playing-field
+  const playingField = document.querySelector('.playing-field');
+  for (let i = 1; i <= levelMakerObject[level]; i++) {
+    // create div w/ class 'hole-area'
+    const holeArea = document.createElement('div');
+    holeArea.setAttribute('class', 'hole-area');
+    // create hole and bunny images
+    const holeImg = document.createElement('img');
+    holeImg.setAttribute('src', 'img/hole.svg');
+    holeImg.setAttribute('class', 'hole');
+    const bunnyImg = document.createElement('img');
+    bunnyImg.setAttribute('src', 'img/bunny.svg');
+    bunnyImg.setAttribute('class', 'bunny');
+    // append images to hole-area
+    holeArea.appendChild(holeImg);
+    holeArea.appendChild(bunnyImg);
+    // append hole-area to playing field
+    playingField.appendChild(holeArea);
+    console.log('making holes')
+    console.log(playingField);
+  }
+
+}
+
 // click on bunny, make it disappear;
 const playingField = document.querySelector('.playing-field');
 playingField.addEventListener('click', (event) => {
@@ -12,3 +49,5 @@ playingField.addEventListener('click', (event) => {
   }
 
 })
+
+makeLevel(1);
