8b195089ad86526176f38f017492facf74e15398
Judy <kimjudyh@gmail.com>
2020-04-05 23:03:06 -0700

    added hole and clickable bunny image

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 console.log('bunny javascript working');
 
 // click on bunny, make it disappear;
-const bunny = document.querySelector('.bunny');
-bunny.addEventListener('click', (event) => {
-  console.log(event.target);
-  bunny.style['display'] = 'none';
+const playingField = document.querySelector('.playing-field');
+playingField.addEventListener('click', (event) => {
+  console.log(event);
+  clickedOn = event.target;
+  console.log(typeof clickedOn);
+  if (clickedOn.className === 'bunny') {
+    console.log('clicked bunny');
+    clickedOn.classList.add('clicked');
+  }
+
 })
